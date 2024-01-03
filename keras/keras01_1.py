54227c96fe46470ae7944bba004abfba3a809af4
[kwi0111] <[okoksk0315@naver.com]>
2024-01-03 21:19:19 +0900

    DAY4

--- keras/keras01_1.py
+++ keras/keras01_1.py
@@ -10,7 +10,7 @@ y = np.array([1,2,3])
 
 #2. 모델 구성
 model = Sequential()    #  순차적인 모델
-model.add(Dense(20, input_dim=1))   
+model.add(Dense(20, input_dim=1))   # Dense : 뉴런의 입출력을 연결 // input_dim : 입력 뉴런의 수를 설정
 model.add(Dense(40))    
 model.add(Dense(60))    
 model.add(Dense(40))    
@@ -18,8 +18,8 @@ model.add(Dense(1))
 
 #3. 컴파일, 훈련 (최적의 weigt)
 model.compile(loss='mse', optimizer='adam')   # loss = 실제값과 예측값 차이. (절대값)낮을수록 좋다 / mse = 제곱하는 방법
-model.fit(x, y, epochs=10000)     # fit 훈련 / epochs 훈련량 -> 최적의 웨이트가 생성
-
+model.fit(x, y, epochs=10000)       # fit 훈련 / epochs 훈련량 -> 최적의 웨이트가 생성
+                                
 #4. 평가, 예측 (최소의 loss)
 loss = model.evaluate(x, y)     # 로스값 평가
 print("로스 : ", loss)
