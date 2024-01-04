ffea3ca77d5ca19daaeea60c584e2ee31876db2e
[kwi0111] <[okoksk0315@naver.com]>
2024-01-04 17:52:49 +0900

    DAY5

--- keras/keras07_scatter2.py
+++ keras/keras07_scatter2.py
@@ -9,9 +9,9 @@ from sklearn.model_selection import train_test_split
 x = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
 y = np.array([1,2,4,3,5,7,9,3,8,12,13, 8,14,15, 9, 6,17,23,21,20])
 
-x_train, x_test, y_train, y_test = train_test_split(x, y,
+x_train, x_test, y_train, y_test = train_test_split(x, y,               # x -> 엑스트레인, 엑스 테스트  y -> 와이 트레인, 와이 테스트 순서 // 순서 바꾸면 이상한 값
                                                     test_size=0.7,
-                                                    random_state=9,
+                                                    random_state=9,     # 랜덤 난수
                                                     shuffle=True,
                                                     )
  
@@ -38,7 +38,7 @@ results = model.predict([x])
 print("로스 : ", loss)
 print("[11]의 예측값", results)
 
-# 모델 구성후 그리기.
+# 모델 구성후 그리기. -> 그래프 그린다.(시각화한다.)
 import matplotlib.pyplot as plt
 plt.scatter(x, y)
 plt.scatter(x, results, c='r', marker = 'x')
@@ -46,6 +46,7 @@ plt.scatter(x, results, c='r', marker = 'x')
 plt.show()
 
 # 파란선 :  실제 데이터 // 빨간선 : 예측 데이터
+
 
 
 
