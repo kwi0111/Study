3c602157701dd0b321805b23893668dd481f933c
kwi0111 <okoksk0315@naver.com>
2024-02-28 21:33:19 +0900

    0228

---
+++ python/project.py
@@ -0,0 +1,89 @@
+import tensorflow as tf
+from keras.layers import Conv2D, BatchNormalization, LeakyReLU, Add, MaxPooling2D, GlobalAveragePooling2D, Dense, Lambda, Concatenate
+import cv2
+import numpy as np
+
+# Convolutional layer with batch normalization and LeakyReLU activation
+def conv_bn_leaky(inputs, filters, kernel_size, strides=(1, 1)):
+    x = Conv2D(filters=filters,
+               kernel_size=kernel_size,
+               strides=strides,
+               padding='same',
+               use_bias=False)(inputs)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.1)(x)
+    return x
+
+# CSP convolution block
+def darknet_conv_block(inputs, filters):
+    x = conv_bn_leaky(inputs, filters // 2, kernel_size=(1, 1))
+    x = conv_bn_leaky(x, filters, kernel_size=(3, 3))
+    return x
+
+# Residual block
+def darknet_residual_block(inputs, filters):
+    x = conv_bn_leaky(inputs, filters // 2, kernel_size=(1, 1))
+    x = conv_bn_leaky(x, filters, kernel_size=(3, 3))
+    x = Add()([inputs, x])
+    return x
+
+# Spatial Pyramid Pooling Module
+def sppf_module(inputs):
+    pool1 = MaxPooling2D(pool_size=(5, 5), strides=(1, 1), padding='same')(inputs)
+    pool2 = MaxPooling2D(pool_size=(9, 9), strides=(1, 1), padding='same')(inputs)
+    pool3 = MaxPooling2D(pool_size=(13, 13), strides=(1, 1), padding='same')(inputs)
+    return Concatenate()([inputs, pool1, pool2, pool3])
+
+# CSP-PAN Module
+def csp_pan_module(inputs):
+    x = conv_bn_leaky(inputs, filters=256, kernel_size=(1, 1))
+    x = conv_bn_leaky(x, filters=512, kernel_size=(3, 3))
+    return x
+
+# YOLOv3 Head
+def yolo_head(inputs, num_classes):
+    x = conv_bn_leaky(inputs, filters=256, kernel_size=(3, 3))
+    x = Conv2D(filters=num_classes, kernel_size=(1, 1), activation='softmax')(x)
+    return x
+
+# New CSP-Darknet53 Backbone with SPPF and CSP-PAN Neck and YOLOv3 Head
+def new_csp_darknet53(input_shape=(416, 416, 3), num_classes=80):
+    inputs = tf.keras.Input(shape=input_shape)
+    
+    # Backbone: New CSP-Darknet53
+    x = conv_bn_leaky(inputs, filters=64, kernel_size=(3, 3))
+    x = darknet_conv_block(x, filters=128)
+    x = darknet_residual_block(x, filters=256)
+    x = darknet_conv_block(x, filters=512)
+    x = darknet_residual_block(x, filters=1024)
+    x = darknet_conv_block(x, filters=2048)
+    
+    # Neck: SPPF, New CSP-PAN
+    x = sppf_module(x)
+    x = csp_pan_module(x)
+    
+    # Head: YOLOv3 Head
+    x = yolo_head(x, num_classes)
+    
+    return tf.keras.Model(inputs, x)
+
+# 클래스 이름 리스트
+class_names = [...]  # 클래스 이름 리스트를 여기에 추가하세요.
+
+# 모델을 로드
+model_path = 'your_model_path.h5'  # 모델이 저장된 경로를 지정하세요.
+model = tf.keras.models.load_model(model_path)
+
+# 이미지를 로드하고 전처리하는 함수
+def preprocess_image(image_path):
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # OpenCV는 이미지를 BGR 형식으로 로드하므로 RGB로 변환합니다.
+    image = cv2.resize(image, (416, 416))  # 모델의 입력 크기로 이미지 크기를 조정합니다.
+    image = image / 255.0  # 이미지를 0과 1 사이의 값으로 정규화합니다.
+    image = np.expand_dims(image, axis=0)  # 배치 차원을 추가합니다.
+    return image
+
+# 모델을 사용하여 객체를 탐지하는 함수
+def detect_objects(model, image):
+    detections = model
+
